38cd96f226ea6600d2b71a549bf68bec39548a28
you88311 <you88311@naver.com>
2022-07-14 23:05:07 +0900

    entr 구현

--- src/Entry.java
+++ src/Entry.java
@@ -2,4 +2,10 @@ class Entry {
     Object key = null;
     Object value = null;
     boolean isDeleted = false;
+
+    public Entry(Object key, Object value, boolean isDeleted) {
+        this.key = key;
+        this.value = value;
+        this.isDeleted = isDeleted;
+    }
 }
